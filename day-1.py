a4afb00367e956eeb4547e53bdf6f4419f5d7299
pilgwon <rlavlfrnjs12@gmail.com>
2021-12-02 01:39:03 +0900

    🎄 DAY 1

---
+++ day-1.py
@@ -0,0 +1,22 @@
+input_file = open('./day-1-input.txt', 'r')
+input_data = input_file.read().rstrip().split('\n')
+
+# problem 1
+delta_list = []
+for index in range(len(input_data)):
+  if index == 0:
+    delta_list.append(0)
+    continue
+  delta = int(input_data[index]) - int(input_data[index-1])
+  delta_list.append(delta)
+result = len(list(filter(lambda x: x > 0, delta_list)))
+
+# problem 2
+delta_list = []
+for index in range(len(input_data)-2):
+  if index == 0: continue
+  delta = int(input_data[index+2]) - int(input_data[index-1])
+  delta_list.append(delta)
+result = len(list(filter(lambda x: x > 0, delta_list)))    
+
+print(result)
